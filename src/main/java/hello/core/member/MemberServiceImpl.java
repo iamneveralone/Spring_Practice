750492f8fb03940e587ec1ad9285a1944d873699
iamneveralone <fieldsawarded@naver.com>
2023-08-13 17:13:46 +0900

    AppConfig를 이용한 코드 수정

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,16 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    // 인터페이스(추상화)에도 의존하고, 구현체에도 의존하는 상황
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 더 이상 MemoryMemberRepository 의존하지 않음
+    // 단지 MemoryRepository 인터페이스만 의존 (추상화에만 의존) -> DIP 지키고 있음
+    private final MemberRepository memberRepository;
+
+    // MemberServiceImpl 입장에서는 생성자를 통해 어떤 구현 객체가 들어올지(주입될지) 알 수 없음
+    // MemberServiceImpl 의 생성자를 통해서 어떤 구현 객체를 주입할지는 오직 외부(AppConfig)에서 결정됨
+    // MemberServiceImpl 은 이제부터 "의존 관계에 대한 고민"은 외부에 맡기고, "실행에만 집중"하면 됨
+    public MemberServiceImpl(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,9 +10,20 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // final 은 필드에서 초기화 or 생성자로 초기화 둘 중 하나
+    // 이 경우는 생성자로 초기화
+    // 인터페이스만 존재 (추상화에만 의존), 구현체에 의존X -> DIP 지키고 있음
+    // discountPolicy 는 FixDiscountPolicy 가 들어올지 RateDiscountPolicy 가 들어올지 모름
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // OrderServiceImpl 입장에서 생성자를 통해 어떤 구현 객체가 들어올지(주입될지) 알 수 없음
+    // OrderServiceImpl 의 생성자를 통해서 어떤 구현 객체를 주입할지는 오직 외부(AppConfig)에서 결정
+    // OrderServiceImpl 은 이제부터 실행에만 집중하면 됨
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
