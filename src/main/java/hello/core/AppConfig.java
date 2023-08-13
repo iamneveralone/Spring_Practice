c5b31eaadfcc4a42f112be33ee9059f0be78109c
iamneveralone <fieldsawarded@naver.com>
2023-08-13 17:24:08 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,13 +13,26 @@ import hello.core.order.OrderServiceImpl;
 // AppConfig 는 생성한 객체 인스턴스의 참조(레퍼런스)를 "생성자를 통해서 주입(연결)"해준다
 // 객체의 생성과 연결은 AppConfig 가 담당
 public class AppConfig {
+    // 리팩터링을 통해 역할과 구현 클래스가 한눈에 들어오도록 수정
+    // -> 애플리케이션 전체 구성이 어떻게 되어 있는지 빠르게 파악할 수 있음
 
-    // 생성자 주입
+    // MemberService 역할
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    // MemberRepository 역할
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // OrderService 역할
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // DiscountPolicy 역할
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
