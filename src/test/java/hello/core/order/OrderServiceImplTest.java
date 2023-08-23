9c0ce0fab5792e33c91b87d0fdb03fe601cdb5d1
iamneveralone <fieldsawarded@naver.com>
2023-08-23 15:19:51 +0900

    @Autowired 생성자 주입 사용 시 순수한 자바 코드를 통한 테스트 용이

--- src/test/java/hello/core/order/OrderServiceImplTest.java
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -1,18 +1,33 @@
 package hello.core.order;
 
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
 
     @Test
     void createOrder(){
-        OrderServiceImpl orderService = new OrderServiceImpl();
-        orderService.createOrder(1L, "itemA", 10000);
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
-    // 현재 OrderServiceImpl 은 @Autowired 생성자 주입 사용 중
-    // 위와 같이 코드 작성 시 컴파일 오류 발생 -> Why? 생성자에 필요한 인자를 넣어주지 않았기 때문
-    // 이 오류를 통해 "아 맞다! 얘는 memberRepository 랑 discountPolicy 가 필요하지!"라고 인지할 수 있음
-    // But, 만약에 수정자(setter) 주입이었으면, null exception 발생 -> 원인 알아내는 과정 까다로움
+
 }
+// 생성자 주입 사용하면 필드에 final 키워드 사용 가능
+// -> 그래서 생성자에서 혹시라도 값이 설정되지 않는 오류를 컴파일 시점에 막아줌
+// -> 즉, 누락된 부분을 쉽게 탐지 가능
+// (중요) 컴파일 오류는 세상에서 가장 빠르고, 좋은 오류다!
+
+// (참고) 수정자(setter) 주입을 포함한 나머지 주입 방식은 모두 생성자 이후에 호출되기 때문에, 필드에 final 키워드 사용 불가
+// -> 오직 생성자 주입 방식만 final 키워드 사용 가능
